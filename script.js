c4c046118803f2bc594443e4fb298b52f735d6ec
IvanFerroli <88122854+IvanFerroli@users.noreply.github.com>
2022-03-02 13:24:40 -0300

    só a formatação dos documentos e a correção de um pequeno bug de CSS, commit feito agora mas alterações feitas dias atrás, certo?

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let featuresSelected = 0;
-let url = 'invalid URL';
-const refImageInput = document.querySelector('#reference-image-input')
-const refImageButton = document.querySelector('#reference-image-button');
+let url = "invalid URL";
+const refImageInput = document.querySelector("#reference-image-input");
+const refImageButton = document.querySelector("#reference-image-button");
 let id = null;
 let model = null;
 let neck = null;
@@ -12,41 +12,42 @@ let author = null;
 let space = "⠀";
 
 function startApp() {
-    userReception();
-    getLastOrders();
+  userReception();
+  getLastOrders();
 }
 startApp();
 
 function userReception() {
-    const userName = prompt('Qual é o seu nome?');
-    if(userName != null && userName != undefined && userName != ''){
-        alert(`Olá ${userName}, seja bem vindo à Fashion Driven!`);
-    }else{
-        startApp();
-    }
-    owner = userName;
-    author = userName;
+  const userName = prompt("Qual é o seu nome?");
+  if (userName != null && userName != undefined && userName != "") {
+    alert(`Olá ${userName}, seja bem vindo à Fashion Driven!`);
+  } else {
+    startApp();
+  }
+  owner = userName;
+  author = userName;
 }
 
 function getLastOrders() {
-    alert("getLastOrders chamada")
-    const promise = axios.get('https://mock-api.driven.com.br/api/v4/shirts-api/shirts');
-    promise.then(renderizeLastOrders)
+  const promise = axios.get(
+    "https://mock-api.driven.com.br/api/v4/shirts-api/shirts"
+  );
+  promise.then(renderizeLastOrders);
 }
 
 function renderizeLastOrders(answer) {
-    let lastOrdersContainer = document.querySelector('.last-orders-container');
-    lastOrdersContainer.innerHTML = '';
-    for(i = 0; i < (answer.data).length; i++){
-        id = answer.data[i].id;
-        model = answer.data[i].model;
-        neck = answer.data[i].neck;
-        material = answer.data[i].material;
-        image = answer.data[i].image;
-        owner = answer.data[i].owner;
-        author = answer.data[i].owner;
-        if(i >= (answer.data.length) - 10){
-            const order = `
+  let lastOrdersContainer = document.querySelector(".last-orders-container");
+  lastOrdersContainer.innerHTML = "";
+  for (i = 0; i < answer.data.length; i++) {
+    id = answer.data[i].id;
+    model = answer.data[i].model;
+    neck = answer.data[i].neck;
+    material = answer.data[i].material;
+    image = answer.data[i].image;
+    owner = answer.data[i].owner;
+    author = answer.data[i].owner;
+    if (i >= answer.data.length - 10) {
+      const order = `
                 <div class="last-order-box" id="${id}" onclick="purchaseFromLastOrders(this)">
                     <div width="180px" height="180px" class="last-orders-image">
                         <img src="${image}" alt="pedido${id}">
@@ -63,175 +64,177 @@ function renderizeLastOrders(answer) {
                         <li class="author-${id}" id="${author}"></li>
                     </ul>
                 </div>
-            `
-            lastOrdersContainer.innerHTML += order;
-        }
-    }    
-}
-
-function selectModel(element) { 
-    const tShirt = document.querySelector('#t-shirt');
-    const tankTop = document.querySelector('#top-tank');
-    const longSleeves = document.querySelector('#long');
-    const selectedModel = element
-    selectedModel.classList.toggle("selected")
-    
-    if(selectedModel == tShirt){
-        tankTop.classList.remove("selected");
-        longSleeves.classList.remove("selected");
-        featuresSelected = featuresSelected + 1;
-    }else if(selectedModel == tankTop){
-        tShirt.classList.remove("selected");
-        longSleeves.classList.remove("selected");
-        featuresSelected = featuresSelected + 1;
-    }else if(selectedModel == longSleeves){
-        tShirt.classList.remove("selected");
-        tankTop.classList.remove("selected");
-        featuresSelected = featuresSelected + 1;
+            `;
+      lastOrdersContainer.innerHTML += order;
     }
-    model = element.id;
-    enableButton();
-    console.log(`Model selected: ${model}`)
+  }
 }
 
-function selectNeck(element) {
-    const vNeck = document.querySelector('#v-neck');
-    const round = document.querySelector('#round');
-    const poloShirt = document.querySelector('#polo');
-    const selectedNeck = element
-    selectedNeck.classList.toggle("selected")
-    
-    if(selectedNeck == vNeck){
-        round.classList.remove("selected");
-        poloShirt.classList.remove("selected");
-        featuresSelected = featuresSelected + 1;
-    }else if(selectedNeck == round){
-        vNeck.classList.remove("selected");
-        poloShirt.classList.remove("selected");
-        featuresSelected = featuresSelected + 1;
-    }else if(selectedNeck == poloShirt){
-        vNeck.classList.remove("selected");
-        round.classList.remove("selected");
-        featuresSelected = featuresSelected + 1;
-    }
-    neck = element.id;
-    enableButton();
-    console.log(`Neck selected: ${neck}`)
-}
+let selectModel = (element) => {
+  const tShirt = document.querySelector("#t-shirt");
+  const tankTop = document.querySelector("#top-tank");
+  const longSleeves = document.querySelector("#long");
+  const selectedModel = element;
+  selectedModel.classList.toggle("selected");
 
-function selectMaterial(element) {
-    const silk = document.querySelector('#silk');
-    const organicCotton = document.querySelector('#cotton');
-    const poliester = document.querySelector('#polyester');
-    const selectedMaterial = element
-    selectedMaterial.classList.toggle("selected")
-    
-    if(selectedMaterial == silk){
-        organicCotton.classList.remove("selected");
-        poliester.classList.remove("selected");
-        featuresSelected = featuresSelected + 1;
-    }else if(selectedMaterial == organicCotton){
-        silk.classList.remove("selected");
-        poliester.classList.remove("selected");
-        featuresSelected = featuresSelected + 1;
-    }else if(selectedMaterial == poliester){
-        silk.classList.remove("selected");
-        organicCotton.classList.remove("selected");
-        featuresSelected = featuresSelected + 1;
-    }
-    material = element.id;
-    enableButton();
-    console.log(`Material selected: ${material}`)
-}
+  if (selectedModel == tShirt) {
+    tankTop.classList.remove("selected");
+    longSleeves.classList.remove("selected");
+    featuresSelected = featuresSelected + 1;
+  } else if (selectedModel == tankTop) {
+    tShirt.classList.remove("selected");
+    longSleeves.classList.remove("selected");
+    featuresSelected = featuresSelected + 1;
+  } else if (selectedModel == longSleeves) {
+    tShirt.classList.remove("selected");
+    tankTop.classList.remove("selected");
+    featuresSelected = featuresSelected + 1;
+  }
+  model = element.id;
+  enableButton();
+};
 
-function checkInputEvent(){
-    console.log(refImageInput.value)
-    enableButton();
+let selectNeck = (element) => {
+  const vNeck = document.querySelector("#v-neck");
+  const round = document.querySelector("#round");
+  const poloShirt = document.querySelector("#polo");
+  const selectedNeck = element;
+  selectedNeck.classList.toggle("selected");
+
+  if (selectedNeck == vNeck) {
+    round.classList.remove("selected");
+    poloShirt.classList.remove("selected");
+    featuresSelected = featuresSelected + 1;
+  } else if (selectedNeck == round) {
+    vNeck.classList.remove("selected");
+    poloShirt.classList.remove("selected");
+    featuresSelected = featuresSelected + 1;
+  } else if (selectedNeck == poloShirt) {
+    vNeck.classList.remove("selected");
+    round.classList.remove("selected");
+    featuresSelected = featuresSelected + 1;
+  }
+  neck = element.id;
+  enableButton();
+};
+
+let selectMaterial = (element) => {
+  const silk = document.querySelector("#silk");
+  const organicCotton = document.querySelector("#cotton");
+  const poliester = document.querySelector("#polyester");
+  const selectedMaterial = element;
+  selectedMaterial.classList.toggle("selected");
+
+  if (selectedMaterial == silk) {
+    organicCotton.classList.remove("selected");
+    poliester.classList.remove("selected");
+    featuresSelected = featuresSelected + 1;
+  } else if (selectedMaterial == organicCotton) {
+    silk.classList.remove("selected");
+    poliester.classList.remove("selected");
+    featuresSelected = featuresSelected + 1;
+  } else if (selectedMaterial == poliester) {
+    silk.classList.remove("selected");
+    organicCotton.classList.remove("selected");
+    featuresSelected = featuresSelected + 1;
+  }
+  material = element.id;
+  enableButton();
+};
+
+function checkInputEvent() {
+  console.log(refImageInput.value);
+  enableButton();
 }
 
 function validateUrl() {
-    if(validURL(refImageInput.value) === true){
-        url = 'valid URL'
-    }else{
-        url = 'invalid URL'
-    }
+  if (validURL(refImageInput.value) === true) {
+    url = "valid URL";
+  } else {
+    url = "invalid URL";
+  }
 }
 
 function validURL(str) {
-    var pattern = new RegExp(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g); // fragment locator
-    return !!pattern.test(str);
-  }
-  
+  var pattern = new RegExp(
+    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+  ); // fragment locator
+  return !!pattern.test(str);
+}
+
 function enableButton() {
-    if(featuresSelected == 3 && url === 'valid URL'){
-        document.getElementById("reference-image-button").disabled = false;
-        refImageButton.classList.add('enable');
-    }else{
-        document.getElementById("reference-image-button").disabled = true;
-        refImageButton.classList.remove('enable');
-    }
-    validateUrl();
+  if (featuresSelected == 3 && url === "valid URL") {
+    document.getElementById("reference-image-button").disabled = false;
+    refImageButton.classList.add("enable");
+  } else {
+    document.getElementById("reference-image-button").disabled = true;
+    refImageButton.classList.remove("enable");
+  }
+  validateUrl();
 }
 
 function purchase() {
-    image = refImageInput.value
-    orderPost();
+  image = refImageInput.value;
+  orderPost();
 }
 
 function purchaseFromLastOrders(element) {
-    const buyingDecision = prompt(`Você deseja fazer o pedido desta peça? (Sim/Não)`);
-    const buyingDecisionLowerCase = buyingDecision.toLocaleLowerCase();
-    const elementId = element.id;
-    model = document.querySelector(`.model-${elementId}`).id;
-    neck = document.querySelector(`.neck-${elementId}`).id;
-    material = document.querySelector(`.material-${elementId}`).id;
-    image = document.querySelector(`.image-${elementId}`).id;
-    owner = document.querySelector(`.owner-${elementId}`).id;
-    author = document.querySelector(`.author-${elementId}`).id;
-    const orderPostObject= {
-        model: `${model}`,
-        neck: `${neck}`,
-        material: `${material}`,
-        image: `${image}`,
-        owner: `${owner}`,
-        author: `${author}`
-    }
-    if(buyingDecisionLowerCase === "sim"){
-    
-        const promise = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', orderPostObject);
-
-        promise.then(() => {
-            alert(`Encomenda confirmada! obrigado por escolher a Fashion Driven!`);
-            getLastOrders();
-        });
-    
-        promise.catch(() => {
-            alert(`Ops, não conseguimos processar a sua encomenda =[`);
-        });
-    }
-}
-
-function orderPost() {
-    const orderPostObject= {
-        model: `${model}`,
-        neck: `${neck}`,
-        material: `${material}`,
-        image: `${image}`,
-        owner: `${owner}`,
-        author: `${author}`
-    }
-    const promise = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', orderPostObject);
+  const buyingDecision = prompt(
+    `Você deseja fazer o pedido desta peça? (Sim/Não)`
+  );
+  const buyingDecisionLowerCase = buyingDecision.toLocaleLowerCase();
+  const elementId = element.id;
+  model = document.querySelector(`.model-${elementId}`).id;
+  neck = document.querySelector(`.neck-${elementId}`).id;
+  material = document.querySelector(`.material-${elementId}`).id;
+  image = document.querySelector(`.image-${elementId}`).id;
+  owner = document.querySelector(`.owner-${elementId}`).id;
+  author = document.querySelector(`.author-${elementId}`).id;
+  const orderPostObject = {
+    model: `${model}`,
+    neck: `${neck}`,
+    material: `${material}`,
+    image: `${image}`,
+    owner: `${owner}`,
+    author: `${author}`,
+  };
+  if (buyingDecisionLowerCase === "sim") {
+    const promise = axios.post(
+      "https://mock-api.driven.com.br/api/v4/shirts-api/shirts",
+      orderPostObject
+    );
 
     promise.then(() => {
-        alert(`Encomenda confirmada! obrigado por escolher a Fashion Driven!`);
-        getLastOrders();
+      alert(`Encomenda confirmada! obrigado por escolher a Fashion Driven!`);
+      getLastOrders();
     });
 
     promise.catch(() => {
-        alert(`Ops, não conseguimos processar a sua encomenda =[`);
+      alert(`Ops, não conseguimos processar a sua encomenda =[`);
     });
+  }
 }
 
+function orderPost() {
+  const orderPostObject = {
+    model: `${model}`,
+    neck: `${neck}`,
+    material: `${material}`,
+    image: `${image}`,
+    owner: `${owner}`,
+    author: `${author}`,
+  };
+  const promise = axios.post(
+    "https://mock-api.driven.com.br/api/v4/shirts-api/shirts",
+    orderPostObject
+  );
 
+  promise.then(() => {
+    alert(`Encomenda confirmada! obrigado por escolher a Fashion Driven!`);
+    getLastOrders();
+  });
 
-
+  promise.catch(() => {
+    alert(`Ops, não conseguimos processar a sua encomenda =[`);
+  });
+}
